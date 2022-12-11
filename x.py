94ffde44b02a722a0576b5aab092e1b4027a384f
claudia2255 <101410856+claudia2255@users.noreply.github.com>
2022-12-11 13:54:50 +0100

    Update x.py

--- x.py
+++ x.py
@@ -1,5 +1,8 @@
-x = 5
-if x<2:
-	print('smaller')
+name = input('What is your name: ')
+pretty = input('From 1 to 10, how handsome are you?: ')
+gf = input ('From 1 to 10, how much do you think your girlfriend loves you?: ')
+msg = 'Hi {name}, welcome to this server. Your input is: you are {pretty} handsome and your girlfriend loves you in scale {gf}. Is this right? Yes or No?: ')
+if msg = 'Yes':
+	print('This input is indeed correct your girlfriend loves you very much and you are very hansome')
 else:
-	print('bigger')
+	print('Well, next time write the truth from the beginning. Your are a selfless bitch. I do not want to code anymore')
