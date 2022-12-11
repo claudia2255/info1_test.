d0c528752959580892e2acc8972c04b2aede69fa
Claudia <claudiadto25@gmail.com>
2022-12-11 13:56:34 +0100

    fixed some bugs

--- x.py
+++ x.py
@@ -1,7 +1,7 @@
 name = input('What is your name: ')
 pretty = input('From 1 to 10, how handsome are you?: ')
 gf = input ('From 1 to 10, how much do you think your girlfriend loves you?: ')
-msg = 'Hi {name}, welcome to this server. Your input is: you are {pretty} handsome and your girlfriend loves you in scale {gf}. Is this right? Yes or No?: ')
+msg = 'Hi {name}, welcome to this server. Your input is: you are {pretty} handsome and your girlfriend loves you in scale {gf}. Is this right? Yes or No?: '
 if msg = 'Yes':
 	print('This input is indeed correct your girlfriend loves you very much and you are very hansome')
 else:
